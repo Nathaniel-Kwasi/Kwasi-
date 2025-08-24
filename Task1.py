b41b79add88661595dd1db533405aa265319a859
Nathaniel-Kwasi <nathanielteinor8@gmail.com>
2025-08-24 14:26:19 +0000

    Delete Task1.py

--- Task1.py
+++
@@ -1,8 +0,0 @@
-intNum=8
-Float_num=8.12
-stringNum="Helloworld"
-bool_Value=False
-print(intNum,"is an:",type(intNum))
-print(Float_num,"is a:",type(Float_num))
-print(stringNum,"is a:",type(stringNum))
-print(bool_Value,"is a:",type(bool_Value))
