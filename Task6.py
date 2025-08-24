37b36ef30ae157d316e5bdfff538af6fc67c5b2e
Nathaniel-Kwasi <nathanielteinor8@gmail.com>
2025-08-24 14:27:45 +0000

    Delete Task6.py

--- Task6.py
+++
@@ -1,3 +0,0 @@
-#print("Hello is a SyntaxError due to the missing closing parenthesis
-#int("abc") is a ValueError, can't convert 'abc' to integer
-#"age"+ 10 is a TypeError, can't concatenate str with int
